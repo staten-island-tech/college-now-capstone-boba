a028dd840c57d79e117b07d2c537a0918c8a3e40
LeoDwi <leod13579@gmail.com>
2024-03-14 16:30:38 -0400

    Update index.js

--- Routes/index.js
+++ Routes/index.js
@@ -4,7 +4,7 @@ const controller = require("../Controllers/controllers");
 const authController = require("../Controllers/authController");
 
 //menu
-router.get("/menu/fetch", controller.getMenuItem);
+router.get("/menu/fetch/:id", controller.getMenuItem);
 router.post("/menu/add", controller.createMenuItem);
 router.patch("/menu/update/:id", controller.updateMenuItem);
 router.delete("/menu/delete/:id", controller.deleteMenuItem);
