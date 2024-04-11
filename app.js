b06240fe1eb0fce4de4b534802ca8fa4bf104664
LeoDwi <leod13579@gmail.com>
2024-04-10 22:51:04 -0400

    Revert "Merge branch 'leo' into kangxi"
    This reverts commit 7448323044c9e432972caf7c26537cb176efe9f0, reversing
    changes made to b523f28887ed7499cbe2d0bce7c8c2b8d5f1cf14.

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
-const cors = require("cors")
 const app = express();
-let corsOptions = {
-origin: "*",
-optionsSuccessStatus: 200,};
-app.use(cors(corsOptions))
 require("./DB/mongoose");
 const routes = require("./Routes/index");
 
--- boba/src/router/index.js
+++ boba/src/router/index.js
@@ -1,23 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import RegisterView from '../views/RegisterView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'register',
-      component: RegisterView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-    },
-    {
-      path: '/home',
       name: 'home',
-      component: () => import('../views/HomeView.vue')
+      component: HomeView
     },
     {
       path: '/about',
