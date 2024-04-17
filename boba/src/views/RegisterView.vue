e5daa7870ba9f64e27cb3db6f9c230cdd12fc88e
LeoDwi <leod13579@gmail.com>
2024-04-17 15:06:00 -0400

    Update RegisterView.vue

--- boba/src/views/RegisterView.vue
+++ boba/src/views/RegisterView.vue
@@ -4,46 +4,19 @@ import { ref } from 'vue'
 
 const store = userStore()
 
-let username = ref()
-let password = ref()
-let zipcode = ref()
+let username = ref('')
+let password = ref('')
+let zipcode = ref('')
 </script>
 
 <template>
   <div>
     <label for="name">Email:</label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      required
-      minlength="4"
-      maxlength="8"
-      size="10"
-      v-model="username"
-    />
+    <input type="text" id="name" name="name" required minlength="4" size="10" v-model="username" />
     <label for="name">Password:</label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      required
-      minlength="4"
-      maxlength="8"
-      size="10"
-      v-model="password"
-    />
+    <input type="text" id="name" name="name" required minlength="4" size="10" v-model="password" />
     <label for="name">Zipcode:</label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      required
-      minlength="4"
-      maxlength="8"
-      size="10"
-      v-model="zipcode"
-    />
+    <input type="text" id="name" name="name" required minlength="4" size="10" v-model="zipcode" />
 
     <button @click="store.$signup(username, password, zipcode)">Submit:</button>
   </div>
